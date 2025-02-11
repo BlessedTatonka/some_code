dc0e7e4737084236aab8acff32cddfb94b2a0e8a
BlessedTatonka <quelquemath@gmail.com>
2025-02-11 14:34:11 +0000

    fix and readme for retromae

--- Training/RetroMAE-ModernBERT/src/run.sh
+++ Training/RetroMAE-ModernBERT/src/run.sh
@@ -3,18 +3,18 @@ torchrun --nproc_per_node 1 -m pretrain.run \
     --model_name_or_path deepvk/moderna-small \
     --model_revision stage_3__50B_tok \
     --output_dir output \
-    --train_data /hdd/TEXT_datasets/RetroMAE_data/ \
+    --train_data /hdd/TEXT_datasets/Wikipedia/ \
     --learning_rate 2e-4 \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 16 \
+    --per_device_train_batch_size 2 \
     --dataloader_drop_last True \
-    --max_seq_length 192 \
+    --max_seq_length 4096 \
     --logging_steps 1 \
     --dataloader_num_workers 4 \
     --report_to wandb \
-    --save_strategy steps \
-    --save_steps 20000 \
+    --save_strategy epoch \
     --warmup_ratio 0.1 \
     --weight_decay 0.01 \
+    --lr_scheduler_type inverse_sqrt \
     --encoder_mlm_probability 0.3 \
     --decoder_mlm_probability 0.5
